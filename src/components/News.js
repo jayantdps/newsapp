e530cf13aedd48b1449d240b0adf9186ff5f153b
Jayant Kaushik <jayantdps@gmail.com>
2021-12-20 18:11:16 +0530

    added date and author
    added date and author

--- src/components/News.js
+++ src/components/News.js
@@ -1,139 +1,136 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
-  articles = [
-    // {
-    //   source: { id: "news24", name: "News24" },
-    //   author: "Compiled by Sibusiso Mjikeliso",
-    //   title:
-    //     "PICTURES | Indian cricket team officially lands in SA for summer tour",
-    //   description:
-    //     "The visiting Indian cricket team was pictured officially landing in South Africa for their summer tour beginning with the Boxing Day Test against the Proteas in Centurion.",
-    //   url: "https://www.news24.com/sport/Cricket/Proteas/pictures-indian-cricket-team-officially-lands-in-sa-for-summer-tour-20211216",
-    //   urlToImage:
-    //     "https://cdn.24.co.za/files/Cms/General/d/372/8899610f02b04c5aaf63f930d759f9ca.jpg",
-    //   publishedAt: "2021-12-16T17:50:46+00:00",
-    //   content:
-    //     "South Africa\r\nDean Elgar (captain, Titans), Temba Bavuma (vice-captain, Lions), Quinton de Kock (wicketkeeper, Titans), Kagiso Rabada (Lions), Sarel Erwee (Dolphins), Beuran Hendricks (Western Provin… [+795 chars]",
-    // },
-    // {
-    //   source: { id: "bbc-sport", name: "BBC Sport" },
-    //   author: "BBC Sport",
-    //   title: "'Ashes at stake one day into second Test'",
-    //   description:
-    //     "There have only been five days' cricket in the Ashes, but England already face being overwhelmed by Australia, writes Stephan Shemilt.",
-    //   url: "http://www.bbc.co.uk/sport/cricket/59657067",
-    //   urlToImage:
-    //     "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/51DA/production/_122245902_joerootandjosbuttler.jpg",
-    //   publishedAt: "2021-12-16T15:07:25.4028633Z",
-    //   content:
-    //     "It started as a dream.\r\nFor a few hours on a cold winter night, English cricket fans could have been forgiven for sleepy thoughts turning to 2005.\r\nEngland had been hammered in the first Ashes Test, … [+4875 chars]",
-    // },
-    // {
-    //   source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-    //   author: null,
-    //   title:
-    //     "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-    //   description:
-    //     "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-    //   url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-    //   urlToImage:
-    //     "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-    //   publishedAt: "2020-04-27T11:41:47Z",
-    //   content:
-    //     "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    // },
-    // {
-    //   source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-    //   author: null,
-    //   title:
-    //     "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-    //   description:
-    //     "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-    //   url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-    //   urlToImage:
-    //     "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-    //   publishedAt: "2020-03-30T15:26:05Z",
-    //   content:
-    //     "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    // },
-  ];
+  static defaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general",
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
   constructor() {
     super();
     this.state = {
-    //   articles: this.articles,
+      //   articles: this.articles,
       articles: [],
       loading: false,
-      page:1
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fa7ce0c23595450db3e3d86aebbababb&page=1&pageSize=${this.props.pageSize}`;
-this.setState({loading: true});
-let data = await fetch(url);
-let parsedData = await data.json()
-this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false})
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fa7ce0c23595450db3e3d86aebbababb&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
   }
 
-   handlePrevClick = async()=> {
-    console.log('next');
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fa7ce0c23595450db3e3d86aebbababb&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-let data = await fetch(url);
-let parsedData = await data.json()
-this.setState({loading: true});
+  handlePrevClick = async () => {
+    console.log("next");
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${this.props.category}&apiKey=fa7ce0c23595450db3e3d86aebbababb&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ loading: true });
     this.setState({
-    page: this.state.page - 1,
-    articles: parsedData.articles,
-    loading: false
-    })
-}
-  
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
 
-   handleNextClick = async()=> {
-    console.log('next');
-    if (!( this.state.page + 1  > Math.ceil(this.state.totalResults/this.props.pageSize))){
- let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fa7ce0c23595450db3e3d86aebbababb&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
- this.setState({loading: true});
-let data = await fetch(url);
-let parsedData = await data.json()
-// this.setState({loading: false});
-    this.setState({
-    page: this.state.page + 1,
-    articles: parsedData.articles,
-    loading: false
-    })
-}
-}
-
-
+  handleNextClick = async () => {
+    console.log("next");
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${this.props.category}&apiKey=fa7ce0c23595450db3e3d86aebbababb&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      // this.setState({loading: false});
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false,
+      });
+    }
+  };
 
   render() {
     return (
       <div className="container my-3">
-          <h1 className="text-center">NewsMonkey - Top Headlines</h1>
-         {this.state.loading && <Spinner />} 
+        <h1 className="text-center" style={{margin: '35px 0px;'}}>NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
         {/* {this.state.articles.map((element)=>{console.log(element)})} */}
         <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4"  key={element.url}>
-                <NewsItem
-                 
-                  title={element.title?element.title.slice(0, 45):""}
-                  description={element.description?element.description.slice(0, 88):""}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+
+                    author={element.author}
+
+                    date={element.publishedAt}
+
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container my-3 d-flex justify-content-between">
-        <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-        <button type="button" disabled={this.state.page + 1  > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,21 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, date, author,source } = this.props;
 
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
+    {source}
+  
+  </span>
           <img src={!imageUrl?"https://image.cnbcfm.com/api/v1/image/104548461-RTX1ILBY.jpg?v=1529452319":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title"> {title}...</h5>
+           
             <p className="card-text">{description}...</p>
+            <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target="_blank" without rel="noreferrer" className="btn btn-sm btn-dark">
               Read more
             </a>
